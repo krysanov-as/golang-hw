5685047e7d0db4f2614d7a132dcf8768a5afa019
Krysanov Alexey <alexey.krysanov@stoloto.ru>
2025-08-30 12:58:52 +0500

    hw03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,42 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	if text == "" {
+		return nil
+	}
+
+	sliceUniqWord := strings.Fields(text)
+
+	maps := make(map[string]int, len(text))
+
+	initCounter := 1
+	for _, v := range sliceUniqWord {
+		_, ok := maps[v]
+
+		if !ok {
+			maps[v] = initCounter
+		} else {
+			maps[v]++
+		}
+	}
+
+	resultSlice := make([]string, 0, len(maps))
+	for w := range maps {
+		resultSlice = append(resultSlice, w)
+	}
+
+	sort.Slice(resultSlice, func(i, j int) bool {
+		iWords, jWords := resultSlice[i], resultSlice[j]
+		if maps[iWords] != maps[jWords] {
+			return maps[iWords] > maps[jWords]
+		}
+		return iWords < jWords
+	})
+
+	return resultSlice[:10]
 }
