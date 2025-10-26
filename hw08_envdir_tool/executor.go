e9e4ebd8960ad9649c7f656a710b4c6ff0e922a4
Krysanov Alexey <alexey.krysanov@stoloto.ru>
2025-10-26 13:13:50 +0500

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,41 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
+const (
+	baseMainError   = 1
+	commandNotFound = 127
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 || cmd[0] == "" {
+		return commandNotFound
+	}
+	// #nosec
+	comand := exec.Command(cmd[0], cmd[1:]...)
+	for k, v := range env {
+		os.Unsetenv(k)
+		if !v.NeedRemove {
+			os.Setenv(k, v.Value)
+		}
+	}
+	comand.Env = os.Environ()
+	comand.Stdin = os.Stdin
+	comand.Stdout = os.Stdout
+	comand.Stderr = os.Stderr
+	err := comand.Run()
+	if err != nil {
+		var errExitCode *exec.ExitError
+		if errors.As(err, &errExitCode) {
+			return errExitCode.ProcessState.ExitCode()
+		}
+		return baseMainError
+	}
+
+	return comand.ProcessState.ExitCode()
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		log.Println("Usage example: go-envdir /path/to/env/dir command arg1 arg2")
+		os.Exit(baseMainError)
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Println("Error reading envdir:", err)
+		os.Exit(baseMainError)
+	}
+
+	returnCode := RunCmd(os.Args[2:], env)
+	os.Exit(returnCode)
 }
